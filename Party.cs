8da0da4d2f2932395c0e8cd41fe8532c86001a65
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-28 10:31:34 -0600

    Update Party.cs

--- Party.cs
+++ Party.cs
@@ -2,19 +2,22 @@
 public class Party
 {
     public string Person {
-       public string construnctor(firstName, lastName) {
+        
+        public string construnctor(firstName, lastName) {
             string firstName = firstName;
             string lastName = lastName;
-     }
-}
+        }
+        
+    }
 
-
- 
+    
     public string Guests = [
         new Person ("Bobby", "Stone"),
         new Person ("Stanly", "Peters"),
         new Person ("Max", "Becker"),
     ];   
+    
 }
+
 
 
