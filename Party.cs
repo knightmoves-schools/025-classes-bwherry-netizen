3233e589912938037ba298cecb935dd435694d47
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-28 10:44:42 -0600

    Update Party.cs

--- Party.cs
+++ Party.cs
@@ -6,13 +6,13 @@ public class Party
             public string lastName = lastName;
         }
         
-    public const Guests = [
-        new Person ("Bobby", "Stone"),
-        new Person ("Stanly", "Peters"),
-        new Person ("Max", "Becker"),
-    ];   
+    public const[] Guests = 
+        new Person ("Bobby", "Stone");
+        new Person ("Stanly", "Peters");
+        new Person ("Max", "Becker");
     
 }
+
 
 
 
