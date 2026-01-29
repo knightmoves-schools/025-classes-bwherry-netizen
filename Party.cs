8bdf890bf6ee07da26955acf69ee7e162e264dbc
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-29 12:02:13 -0600

    Update Party.cs

--- Party.cs
+++ Party.cs
@@ -2,25 +2,26 @@
 
 public class Party
 {      
-         public Person[] Guests = new Person[] 
-         {
-                new Person("Dave", "Miller"),
-                new Person("Jack", "Kennedy"),
-                new person("Harry", "Fitzgerald")        
-        };
+    public Person[] Guests = new Person[] 
+    {
+        new Person("Dave", "Miller"),
+        new Person("Jack", "Kennedy"),
+        new person("Harry", "Fitzgerald")        
+    };
 }
 
 public class Person 
 {
-        public string firstName { get; set; }
-        public string lastname { get; set; }
+    public string FirstName { get; set; }
+    public string Lastname { get; set; }
         
-        public Person (string firstName, string lastName) 
+    public Person (string firstName, string lastName) 
         {
                 FirstName = firstName;
                 LastName = lastName;
         }
 }  
+
 
 
 
