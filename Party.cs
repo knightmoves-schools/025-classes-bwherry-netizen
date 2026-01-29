57cf09c9dd48221cd20fd3a87da995cdc76d1cd2
Brendon W. <bwherry@midland.k12.ia.us>
2026-01-29 11:44:02 -0600

    Update Party.cs

--- Party.cs
+++ Party.cs
@@ -1,38 +1,17 @@
 ﻿namespace knightmoves;
 public class Party
 {      
-        public void Guests () {
-                Person firstGuest = new Person("Dave", "Miller");
-                Person seccondGuest = new Person("Jack", "Kennedy");
-                Person thirdGuest = new person("Harry", "Fitzgerald");
-
-
-                
-        }
-        
-
-        public Party.Guests()
-
         public class Person (string firstName, string lastName) {
                 this.firstName = firstName;
                 this.lastname = lastName;
-        }            
+        }  
+        
+        public void Guests () {
+                Person firstGuest = new Person("Dave", "Miller");
+                Person seccondGuest = new Person("Jack", "Kennedy");
+                Person thirdGuest = new person("Harry", "Fitzgerald");        
+        }
+        
+
+        public Party.Guests()          
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
